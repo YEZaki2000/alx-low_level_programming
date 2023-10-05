2f21cc2ff837a87bb62f31a4428fa5f0ae83421e
Yehia <yahia.e.zaki@gmail.com>
2023-10-05 17:33:22 +0000

    Pushing all tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * Program to print "Programming is like building a multilingual puzzle
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle? \n");
+
+	return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+include <stdio.h>
+
+/*
+Program to print "with proper grammar, but the outcome is a piece of art,"
+*/
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+
+	return 0;
+}
