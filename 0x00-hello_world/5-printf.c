f943be5e4542e38e5361f8ded6a2d58b6c7572eb
Yahia Essam <99550520+YEZaki2000@users.noreply.github.com>
2023-10-05 21:17:43 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 /**
-*main - prints "with proper grammar, 
+*main - prints "with proper grammar,
 *but the outcome is a piece of art,"
-*return 0.
+*
+*Return: 0.
 */
 
 int main(void)
