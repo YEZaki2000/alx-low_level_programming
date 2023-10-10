a81823aaa2fa74ebda35dc40e8759dff5ea1524f
Yehia <yahia.e.zaki@gmail.com>
2023-10-11 00:13:33 +0300

    added _putchar file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,9 @@
 
 void print_alphabet_x10(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+
+	for (i = 0; i < 10; i++)
 	{
 		char ch;
 		for (ch = 'a'; ch <= 'z'; ch++)
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* _putchar() - passes the arguments into
+* the library putchar() function and returns
+* the result.
+*
+* Return: The result of the library
+* putchar() function.
+*/
+
+int _putchar(int ch)
+{
+	return putchar(ch);
+}
