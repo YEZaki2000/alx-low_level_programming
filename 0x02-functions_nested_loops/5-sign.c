c7f5ffc3941a73b7c97189da17c809cdaab28cf1
Yehia <yahia.e.zaki@gmail.com>
2023-10-11 15:10:12 +0300

    jdkfjd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
 * print_sign() - prints the sign of a number.
 *
@@ -8,12 +10,15 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		return -1;
+		printf("-");
+		return (-1);
 	} else if (n > 0)
 	{
-		return +1;
+		printf("+");
+		return (1);
 	} else
 	{
-		return (00);
+		printf("0");
+		return (0);
 	}
 }
