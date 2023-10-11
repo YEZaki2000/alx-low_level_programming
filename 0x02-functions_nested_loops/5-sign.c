d348675aed6124216721e25f9dc3ab8674888f2f
Yehia <yahia.e.zaki@gmail.com>
2023-10-11 15:15:56 +0300

    jdkfjd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,15 +10,15 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		printf("-");
+		printf("-1");
 		return (-1);
 	} else if (n > 0)
 	{
-		printf("+");
+		printf("+1");
 		return (1);
 	} else
 	{
-		printf("0");
+		printf("00");
 		return (0);
 	}
 }
