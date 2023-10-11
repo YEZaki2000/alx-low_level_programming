479894f9170e050328ba5a709a149471a361a58f
Yehia <yahia.e.zaki@gmail.com>
2023-10-11 15:24:39 +0300

    jdkfjd

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,15 +10,15 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		putchar('-');
+		putchar(45);
 		return (-1);
 	} else if (n > 0)
 	{
-		putchar('+');
+		putchar(43);
 		return (1);
 	} else
 	{
-		putchar('0');
+		putchar(48);
 		return (0);
 	}
 }
