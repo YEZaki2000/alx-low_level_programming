084a931fbec83b2cef3045636b2ced8e2575a8b3
Yehia <yahia.e.zaki@gmail.com>
2023-10-11 17:49:03 +0300

    ksjfsl

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
-* print_alphabet_x10() - jfksdjfksldjf
+* print_alphabet_x10() - prints 10 times the alphabet,
+* in lowercase, followed by a new line.
 *
 * Return: Null.
 */
