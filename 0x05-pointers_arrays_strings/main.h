31a4d52de4ef6332cb4c268d227c3d53058aea71
Yehia <yahia.e.zaki@gmail.com>
2023-10-16 23:01:08 +0300

    masdjkf

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
-int _putchar(char c)
+#include <stdio.h>
+int _putchar(char)
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+int _strlen(char);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
