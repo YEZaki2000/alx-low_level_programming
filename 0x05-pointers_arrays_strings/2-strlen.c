e67c16de93a131763319571cc2c0c07b95264ab1
Yehia <yahia.e.zaki@gmail.com>
2023-10-16 22:58:42 +0300

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns
+ * the length of a string.
+ * @s: input string.
+ * Return: length of the string.
+ */
+
+
+int _strlen(char *s)
+{
+	return (strln(*s));
+}
