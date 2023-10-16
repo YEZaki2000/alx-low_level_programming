105e10ad9084a38cb861b601860c24fb60e1b216
Yehia <yahia.e.zaki@gmail.com>
2023-10-16 21:45:19 +0300

    lskdjf

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+* void reset_to_98 - takes a pointer to an int as parameter
+* and updates the value it points to to 98.
+* @*n: input pointer.
+* Return: Null.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
