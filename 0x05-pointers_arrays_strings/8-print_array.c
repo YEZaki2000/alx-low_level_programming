37cd7d338b2b170ad20bd2aedbf12cfe831797aa
Yehia <yahia.e.zaki@gmail.com>
2023-10-17 23:38:15 +0300

    lksjdf

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,14 +14,12 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
 		if (i < (n - 1))
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", a[i]);
 		} else
 		{
-			_putchar('\n');
+			printf("%d\n", a[i]);
 		}
 	}
 }
