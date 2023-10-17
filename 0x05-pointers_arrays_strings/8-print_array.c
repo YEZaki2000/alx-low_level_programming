e1d27954b21c0774387cd95c4a7e255956e055c2
Yehia <yahia.e.zaki@gmail.com>
2023-10-17 23:29:39 +0300

    lskdfjksld

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements
+ * of an array of integers, followed by a new line.
+ * @a: input array.
+ * @n: input num.
+ * Return: Null.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+		if (i < (n - 1))
+		{
+			_putchar(',');
+			_putchar(' ');
+		} else
+		{
+			_putchar('\n');
+		}
+	}
+}
