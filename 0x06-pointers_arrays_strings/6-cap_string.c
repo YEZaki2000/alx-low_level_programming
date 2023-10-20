54606d49e02976a7a6197d48d86d3a5527952d18
Yehia <yahia.e.zaki@gmail.com>
2023-10-20 22:17:37 +0300

    lksdjf

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,11 +12,12 @@ char *cap_string(char *c)
 	char *space = " ";
 	char *tab = "\t";
 	char *dot = ".";
+	char *newline = "\n";
 
 	lngh = strlen(c);
 	for (i = 0; i < lngh; i++)
 	{
-		if ((i == 0) || (c[i - 1] == *space) || (c[i - 1] == *tab) || (c[i - 1] == *dot))
+		if ((i == 0) || (c[i - 1] == *space) || (c[i - 1] == *tab) || (c[i - 1] == *dot) || (c[i - 1] == *newline))
 		{
 			c[i] = toupper(c[i]);
 		}
