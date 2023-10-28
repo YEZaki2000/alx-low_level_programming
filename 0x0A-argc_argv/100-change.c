fa20d0d5a25be400a551bf77b8eee27fb11851bb
Yehia <yahia.e.zaki@gmail.com>
2023-10-28 04:16:55 +0200

    lksjdf

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,14 +12,23 @@ int main(int argc, char *argv[])
 {
 	int i, res, counter, cents;
 	int coins[] = {25, 10, 5, 2, 1};
-	(void)argc;
 	cents = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
 	res = 0;
 	counter = 0;
 
 	for (i = 0; i <= 5; i++)
 	{
+		if (cents < 0)
+		{
+			printf("0\n");
+			break;
+		}
 		if (res > cents)
 		{
 			res -= coins[i - 1];
