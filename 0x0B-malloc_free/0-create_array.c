1e81cb30f7d220e90c736cd18716c4d54045ae7a
Yehia <yahia.e.zaki@gmail.com>
2023-10-31 23:00:07 +0200

    ksjdf

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,9 @@
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	str = malloc(sizeof(char) * size);
+	str[0] = c;
+
+	return (str);
+}
---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * argstostr - lhskjdf
+ * @ac: lskjdf
+ * @av: lksjdf
+ * Return: lksjdf
+ */
+
+char *argstostr(int ac, char **av)
+{
+	(void)ac;
+	char *s[100];
+
+	strcpy(s, *av);
+
+	return (s);
+}
