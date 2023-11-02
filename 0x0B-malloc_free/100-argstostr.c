0087289b9ef777e015c12c06764ce2505f4ac5d2
Yehia <yahia.e.zaki@gmail.com>
2023-11-02 05:34:05 +0200

    lskjdf

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,10 +9,41 @@
 
 char *argstostr(int ac, char **av)
 {
-	(void)ac;
-	char *s[100];
+	char *s;
+	/*char *temp;*/
+	int i, j, lengthbefore;
+	/* int x; */
 
-	strcpy(s, *av);
+	/*
+	temp = malloc(sizeof(av));
+	for (x = 0; x < ac; x++)
+	{
+		char length = strcat("lngh", itoa(x));
+		strcpy(temp[x], length);
+	}
+	*/
+
+	s = malloc(sizeof(av) * 10);
+	strcpy(s, av[0]);
+
+	lengthbefore = 0;
+	for (i = 1; i < ac; i++)
+	{
+		printf("current arg%s\n", av[i]);
+		/*char lengthbefore = strcat("lngh", itoa(i - 1));*/
+		lengthbefore += strlen(av[i - 1]);
+		printf("%d\n", lengthbefore);
+		int length = strlen(av[i]);
+		for (j = 0; j < length; j++)
+		{
+			printf("%c\n", av[i][j]);
+			printf("s: %c\n", s[j]);
+			s[lengthbefore + j] = av[i][j];
+			printf("slengthbefore + j: %c\n", s[lengthbefore + j]);
+		}
+		printf("s now: %s\n", s);
+		/*lengthbefore += length;*/
+	}
 
 	return (s);
 }
