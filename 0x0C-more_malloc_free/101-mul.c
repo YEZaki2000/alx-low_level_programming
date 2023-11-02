cc7f2a18a336a6162bb2e152c0a3dc710d9273e8
Yehia <yahia.e.zaki@gmail.com>
2023-11-03 01:52:58 +0200

    ksjdf

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - ksldgf
+ * @argc: lksjdf
+ * @argv: lksjdf
+ * Return: jklsdjf
+ */
+
+void main(int argc, char *argv[])
+{
+	int res;
+
+	res = (atoi(argv[1]) * atoi(argv[2]));
+	putchar(res);
+}
