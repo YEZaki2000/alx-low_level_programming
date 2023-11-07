bc1e6d7ec3539e93fba5cdf314d13ece58db425b
Yehia <yahia.e.zaki@gmail.com>
2023-11-07 05:55:41 +0200

    lksjdf

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * new_dog - lksjdf
+ * @name: lkjsdf
+ * @age: lksjdf
+ * @owner: lkjsdf
+ * Return: lskjdf
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t myveryowndog;
+
+	dog_t.name = name;
+	dog_t.age = age;
+	dog_t.owner = owner;
+
+	return (myveryowndog);
+}
