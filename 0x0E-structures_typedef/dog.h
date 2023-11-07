75fdedb61def86a7bc9f794d32566c40be6bb133
Yehia <yahia.e.zaki@gmail.com>
2023-11-07 04:46:38 +0200

    l;ksjdf\

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog
+#define dog
+
+/**
+ * struct dog - lksjdf
+ * @name: lksjdf
+ * @age: lskjdf
+ * @owner: lksjdf
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
