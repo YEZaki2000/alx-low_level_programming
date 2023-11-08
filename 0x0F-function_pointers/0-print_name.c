6e3f9786ede311e24935dd13dd3bffcd52e71c1e
Yehia <yahia.e.zaki@gmail.com>
2023-11-08 16:42:27 +0200

    l;ksjdf

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - lkjsdf
+ * @name: lkjsdf
+ * @f: lksjdf
+ * Return: lskdjf
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
