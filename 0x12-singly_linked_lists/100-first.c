2cba875466a7777b20436b5687c516815a25ee88
Yehia <yahia.e.zaki@gmail.com>
2023-11-17 00:47:18 +0200

    lksjdf

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - lksjdf
+ * Return: lskjfd
+ */
+
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+void before_main() __attribute__((constructor));
+
+#endif
